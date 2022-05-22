dfd4e17a7a36331ed8c6475baefe8bd8ee08e20f
Alexandre Vieira <101663403+ALEavds@users.noreply.github.com>
2022-05-21 22:54:53 -0300

    Update script_biblioteca.sql

--- script_biblioteca.sql
+++ script_biblioteca.sql
@@ -1,7 +1,7 @@
-drop table emprestimo;
+/*drop table emprestimo;
 drop table livro;
 drop table usuario;
-
+*/
 create table livro(
 id_livro serial primary key,
 titulo varchar (100) NOT NULL
@@ -13,13 +13,14 @@ idade integer
 );
 create table emprestimo(
 id_emprestimo serial primary key,
-id_usuario integer,
-id_livro integer,
-data_emprestimo varchar(10),
-constraint fk_UsuEmp foreign key (id_usuario) references usuario (id_usuario),
-constraint fk_LivEmp foreign key (id_livro) references livro (id_livro)
+id_usuario integer unique,
+id_livro integer unique,
+data_emprestimo date
 );
-
+alter table emprestimo
+add foreign key (id_usuario) references usuario(id_usuario);
+alter table emprestimo
+add foreign key (id_livro) references livro(id_livro);
 insert into livro (titulo) values ('Engenharia de software');
 insert into livro (titulo) values ('Segunda Guerra');
 insert into livro (titulo) values ('Sistemas distribuidos');
@@ -42,48 +43,49 @@ insert into usuario (nome, idade) values ('Julia',15);
 insert into usuario (nome, idade) values ('Laura',17);
 insert into usuario (nome, idade) values ('Bruno',29);
 
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (1, 2, '12/05');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (3, 3, '22/04');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (5, 1, '11/05');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (2, 5, '25/04');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (6, 4, '12/05');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (4, 7, '07/06');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (8, 9, '18/05');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (9, 10, '19/05');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (7, 6, '30/04');
-insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (10, 8, '03/06');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (1, 2, '2022-05-12');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (3, 3, '2022-04-22');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (5, 1, '2022-05-11');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (2, 5, '2022-04-25');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (6, 4, '2022-05-12');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (4, 7, '2022-06-07');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (8, 9, '2022-05-18');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (9, 10, '2022-05-19');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (7, 6, '2022-04-30');
+insert into emprestimo (id_usuario, id_livro, data_emprestimo) values (10, 8, '2022-06-03');
 
 
+/*--Livros emprestados e seus respectivos usuários--*/
+
+select u.nome, l.titulo from emprestimo as e
+inner join usuario as u on u.id_usuario = e.id_usuario
+inner join livro as l on l.id_livro = e.id_livro;
 
 
+/*--Livros emprestados para menores de idade--*/
+
+select u.nome, u.idade, l.titulo from emprestimo as e
+inner join usuario as u on u.id_usuario = e.id_usuario
+inner join livro as l on l.id_livro = e.id_livro
+where idade<18;
 
 
+/*--Livros emprestados para maiores de idade--*/
+
+select u.nome, u.idade, l.titulo from emprestimo as e
+inner join usuario as u on u.id_usuario = e.id_usuario
+inner join livro as l on l.id_livro = e.id_livro
+where u.idade>=18;
 
 
+/*--Livros emprestados durante o mês 05--*/
+
+select u.nome, l.titulo, e.data_emprestimo from emprestimo as e
+inner join usuario as u on u.id_usuario = e.id_usuario
+inner join livro as l on l.id_livro = e.id_livro
+where e.data_emprestimo between '2022-05-01' and '2022-05-31';
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-select * from emprestimo as e
-inner join usuario as u
-
-where idade >= 18
-;
-
-
-
-/*
-select * from livro;
-select * from usuario;
-select * from emprestimo;
-*/
+/*--Livros que começam com a letra S--*/
+select * from livro
+where titulo Like 'S%';
